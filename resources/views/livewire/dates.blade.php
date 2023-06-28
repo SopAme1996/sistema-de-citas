b7599c5b1b8a7987b0a2ccd4a318cb32748c76c7
Mario Ciau <mario_josue1110@hotmail.com>
2023-06-27 22:23:36 -0600

    moficacion wizard

--- resources/views/livewire/dates.blade.php
+++ resources/views/livewire/dates.blade.php
@@ -1,6 +1,6 @@
 
 @section('extra.css')
-<link id="pagestyle" href="{{ asset('assets') }}/css/bootstrap.min.css" rel="stylesheet" />
+{{-- <link id="pagestyle" href="{{ asset('assets') }}/css/bootstrap.min.css" rel="stylesheet" /> --}}
 <link id="pagestyle" href="{{ asset('assets') }}/css/material-bootstrap-wizard.css" rel="stylesheet" />
 
 {{-- <link id="pagestyle" href="{{ asset('assets') }}/css/material-dashboard.css?v=3.0.0" rel="stylesheet" /> --}}
@@ -44,177 +44,51 @@
                             <div class="tab-content">
                                 <div class="tab-pane" id="details">
                                     <div class="row">
-                                        <div class="col-sm-12">
-                                            <h4 class="info-text"> Let's start with the basic details.</h4>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <div class="input-group">
-                                                <span class="input-group-addon">
-                                                    <i class="material-icons">email</i>
-                                                </span>
-                                                <div class="form-group label-floating">
-                                                      <label class="control-label">Your Email</label>
-                                                      <input name="name" type="text" class="form-control">
+                                        <div class="card">
+                                            <div class="card-header mx-4 p-3 text-center">
+                                                <div class="icon icon-shape icon-lg bg-gradient-primary shadow text-center border-radius-lg">
+                                                    <i class="material-icons opacity-10">account_balance</i>
                                                 </div>
                                             </div>
-
-                                            <div class="input-group">
-                                                <span class="input-group-addon">
-                                                    <i class="material-icons">lock_outline</i>
-                                                </span>
-                                                <div class="form-group label-floating">
-                                                      <label class="control-label">Your Password</label>
-                                                      <input name="name2" type="password" class="form-control">
-                                                </div>
-                                            </div>
-
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <div class="form-group label-floating">
-                                                <label class="control-label">Country</label>
-                                                <select class="form-control">
-                                                    <option disabled="" selected=""></option>
-                                                    <option value="Afghanistan"> Afghanistan </option>
-                                                    <option value="Albania"> Albania </option>
-                                                    <option value="Algeria"> Algeria </option>
-                                                    <option value="American Samoa"> American Samoa </option>
-                                                    <option value="Andorra"> Andorra </option>
-                                                    <option value="Angola"> Angola </option>
-                                                    <option value="Anguilla"> Anguilla </option>
-                                                    <option value="Antarctica"> Antarctica </option>
-                                                    <option value="...">...</option>
-                                                </select>
-                                            </div>
-                                            <div class="form-group label-floating">
-                                                <label class="control-label">Daily Budget</label>
-                                                <select class="form-control">
-                                                    <option disabled="" selected=""></option>
-                                                    <option value="Afghanistan"> < $100 </option>
-                                                    <option value="Albania"> $100 - $499 </option>
-                                                    <option value="Algeria"> $499 - $999 </option>
-                                                    <option value="American Samoa"> $999+ </option>
-                                                </select>
+                                            <div class="card-body pt-0 p-3 text-center">
+                                                <h6 class="text-center mb-0">Salary</h6>
+                                                <span class="text-xs">Belong Interactive</span>
+                                                <hr class="horizontal dark my-3">
+                                                <h5 class="mb-0">+$2000</h5>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="tab-pane" id="captain">
-                                    <h4 class="info-text">What type of room would you want? </h4>
-                                    <div class="row">
-                                        <div class="col-sm-10 col-sm-offset-1">
-                                            <div class="col-sm-4">
-                                                <div class="choice" data-toggle="wizard-radio" rel="tooltip" title="This is good if you travel alone.">
-                                                    <input type="radio" name="job" value="Design">
-                                                    <div class="icon">
-                                                        <i class="material-icons">weekend</i>
-                                                    </div>
-                                                    <h6>Single</h6>
-                                                </div>
-                                            </div>
-                                            <div class="col-sm-4">
-                                                <div class="choice" data-toggle="wizard-radio" rel="tooltip" title="Select this room if you're traveling with your family.">
-                                                    <input type="radio" name="job" value="Code">
-                                                    <div class="icon">
-                                                        <i class="material-icons">home</i>
-                                                    </div>
-                                                    <h6>Family</h6>
-                                                </div>
-                                            </div>
-                                            <div class="col-sm-4">
-                                                <div class="choice" data-toggle="wizard-radio" rel="tooltip" title="Select this option if you are coming with your team.">
-                                                    <input type="radio" name="job" value="Code">
-                                                    <div class="icon">
-                                                        <i class="material-icons">business</i>
-                                                    </div>
-                                                    <h6>Business</h6>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    <h4 class="info-text">Colaboradores </h4>
+                          
                                 </div>
                                 <div class="tab-pane" id="description">
                                     <div class="row">
-                                        <h4 class="info-text"> Drop us a small description.</h4>
-                                        <div class="col-sm-6 col-sm-offset-1">
-                                            <div class="form-group">
-                                                <label>Room description</label>
-                                                <textarea class="form-control" placeholder="" rows="6"></textarea>
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-4">
-                                            <div class="form-group">
-                                                <label class="control-label">Example</label>
-                                                <p class="description">"The room really nice name is recognized as being a really awesome room. We use it every sunday when we go fishing and we catch a lot. It has some kind of magic shield around it."</p>
-                                            </div>
-                                        </div>
+                                        <h4 class="info-text"> Seleccione un servicio.</h4>
+                                   
                                     </div>
                                 </div>
                                 <div class="tab-pane" id="dates">
                                     <div class="row">
-                                        <h4 class="info-text"> Drop us a small description.</h4>
-                                        <div class="col-sm-6 col-sm-offset-1">
-                                            <div class="form-group">
-                                                <label>Room description</label>
-                                                <textarea class="form-control" placeholder="" rows="6"></textarea>
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-4">
-                                            <div class="form-group">
-                                                <label class="control-label">Example</label>
-                                                <p class="description">"The room really nice name is recognized as being a really awesome room. We use it every sunday when we go fishing and we catch a lot. It has some kind of magic shield around it."</p>
-                                            </div>
-                                        </div>
+                                        <h4 class="info-text"> Horarios disponibles.</h4>
+
                                     </div>
                                 </div>
                                 <div class="tab-pane" id="information">
                                     <div class="row">
-                                        <h4 class="info-text"> Drop us a small description.</h4>
-                                        <div class="col-sm-6 col-sm-offset-1">
-                                            <div class="form-group">
-                                                <label>Room description</label>
-                                                <textarea class="form-control" placeholder="" rows="6"></textarea>
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-4">
-                                            <div class="form-group">
-                                                <label class="control-label">Example</label>
-                                                <p class="description">"The room really nice name is recognized as being a really awesome room. We use it every sunday when we go fishing and we catch a lot. It has some kind of magic shield around it."</p>
-                                            </div>
-                                        </div>
+                                        <h4 class="info-text"> Llene los campos requeridos.</h4>
+
                                     </div>
                                 </div>
                                 <div class="tab-pane" id="buy">
                                     <div class="row">
-                                        <h4 class="info-text"> Drop us a small description.</h4>
-                                        <div class="col-sm-6 col-sm-offset-1">
-                                            <div class="form-group">
-                                                <label>Room description</label>
-                                                <textarea class="form-control" placeholder="" rows="6"></textarea>
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-4">
-                                            <div class="form-group">
-                                                <label class="control-label">Example</label>
-                                                <p class="description">"The room really nice name is recognized as being a really awesome room. We use it every sunday when we go fishing and we catch a lot. It has some kind of magic shield around it."</p>
-                                            </div>
-                                        </div>
+                                        <h4 class="info-text"> Procesar pago.</h4>
                                     </div>
                                 </div>
                                 <div class="tab-pane" id="success">
                                     <div class="row">
-                                        <h4 class="info-text"> Drop us a small description.</h4>
-                                        <div class="col-sm-6 col-sm-offset-1">
-                                            <div class="form-group">
-                                                <label>Room description</label>
-                                                <textarea class="form-control" placeholder="" rows="6"></textarea>
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-4">
-                                            <div class="form-group">
-                                                <label class="control-label">Example</label>
-                                                <p class="description">"The room really nice name is recognized as being a really awesome room. We use it every sunday when we go fishing and we catch a lot. It has some kind of magic shield around it."</p>
-                                            </div>
-                                        </div>
+                                        <h4 class="info-text"> Confirmar Cita.</h4>
                                     </div>
                                 </div>
                             </div>
@@ -232,7 +106,7 @@
                                               <label>
                                                   <input type="checkbox" name="optionsCheckboxes">
                                               </label>
-                                              Subscribe to our newsletter
+                                            Recordar datos
                                           </div>
                                       </div>
                                     </div>
